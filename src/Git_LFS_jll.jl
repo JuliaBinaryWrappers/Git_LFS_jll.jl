8222985f15812c324413507817ca550f08d6c3b6
jlbuild <juliabuildbot@gmail.com>
2025-07-11 14:23:13 +0000

    Git_LFS_jll build 3.7.0+0

---
+++ src/Git_LFS_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule Git_LFS_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("Git_LFS")
+JLLWrappers.@generate_main_file("Git_LFS", UUID("020c3dae-16b3-5ae5-87b3-4cb189e250b2"))
+end  # module Git_LFS_jll
